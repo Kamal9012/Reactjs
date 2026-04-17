434921e0db004a8b1c2bc5e791be4352749beaa0
kamalthapa <kamalthapa.nitv@gmail.com>
2026-04-17 10:09:46 +0545

    first commit

---
+++ app/page.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <div className="flex">
+      <div className="flex flex-col justify-start bg-red-200 h-100 w-200 m-10 box-border size-32  rounded-xl shadow-xl">
+        <Image className="rounded-sm  " height={80} width={80} src="/boyimage.webp" alt="image"/>
+        <div className="text-blue-300 text-2xl ">kamal thapa</div>
+        
+      
+      </div>
+
+        <div className="bg-blue-200 h-100 w-200 m-10 rounded-xl shadow-sm"></div>
+      </div>
+
+      <div className="flex">
+        <div className="bg-yellow-200 h-100 w-200 m-10 rounded-sm shadow-xl"></div>
+
+        <div className="bg-green-200 h-100 w-200 m-10 rounded-xl shadow-xl"></div>
+      </div>
+    </div>
+  );
+}
